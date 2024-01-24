65d84d162540166c700ab5e59ce6663302e172bb
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2024-01-24 10:13:36 +0200

    Implement hash function

---
+++ src/hash.js
@@ -0,0 +1,12 @@
+export default function hash(string) {
+  let hash = 0;
+  if (string.length == 0) {
+      return hash;
+  };
+  for (var i = 0; i < string.length; i++) {
+      let char = string.charCodeAt(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash; // Convert to 32bit integer
+  };
+  return hash;
+};
