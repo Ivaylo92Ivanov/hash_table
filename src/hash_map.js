81d4f87961b41d564e0d6fc1cee5a7444bf63101
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2024-01-25 10:32:55 +0200

    Implement clear HashMap function

--- src/hash_map.js
+++ src/hash_map.js
@@ -5,18 +5,9 @@ export default class HashMap {
   constructor (capacity) {
     this.capacity = capacity;
     this.bucketArray = [];
-    for (let i=0; i<capacity; i++) { this.bucketArray.push({}) };
+    for (let i=0; i<this.capacity; i++) { this.bucketArray.push({}) };
 
-    this.checkLoadFactor = () => {
-      let nodeCount = 0;
-      this.bucketArray.forEach((node) => { if(Object.keys(node).length>0) {nodeCount++;} });
-      console.log(nodeCount)
-      console.log(nodeCount/this.capacity)
-      if ((nodeCount/this.capacity) > this.#load_factor) {
-        //grow capacity and redistribute all nodes accordingly
-        this.capacity
-      };
-    };
+
 
     this.set = (key, value) => {
       const hashCode = hash(key);
@@ -37,7 +28,6 @@ export default class HashMap {
         while (tempObj) {
           if (hashCode==hash(tempObj.key)) { 
             tempObj.value = value; 
-            
             isPlaced = true;
             break; };
           lastObj=tempObj;
@@ -50,12 +40,77 @@ export default class HashMap {
             'next': null
           };
       };
-      // check load capacity
-      console.log(JSON.stringify(this.bucketArray))
+      // check load capacity and grow hash map if necessary
+      this.bucketArray.forEach(node => console.log(node));
+      console.log('***************************');
       this.checkLoadFactor();
     };
 
+    this.checkLoadFactor = () => {
+      let nodeCount = 0;
+      this.bucketArray.forEach((node) => { if(Object.keys(node).length>0) {nodeCount++;} });
+      if((nodeCount/this.capacity)>this.#load_factor) this.growHashMap();
+    };
 
-  }
-}
+    this.growHashMap = () => {
+      const oldBucketArray = this.bucketArray;
+      this.bucketArray = [];
+      this.capacity*=2;
+      for (let i=0; i<this.capacity; i++) { this.bucketArray.push({}) };
+      console.log('--------------------------')
+      oldBucketArray.forEach((mapNode) => {
+        // if node is not empty iterate over its key-value pairs and add them to a keyValuePairsFromNode array
+        if(Object.keys(mapNode).length>0) {
+          let keyValuePairsFromNode = [];
+          while (mapNode) {             
+            keyValuePairsFromNode.push([mapNode.key, mapNode.value]);
+            mapNode = mapNode.next;
+          };
+          // add each key-value pair in the array, to the new, grown hashMap
+          keyValuePairsFromNode.forEach((keyValuePair) => {
+            let key = keyValuePair[0];
+            let value = keyValuePair[1];
+            this.set(key, value);
+          });
+        };
+      });
+    };
+
+    this.get = () => {
+
+    }
+    
+    this.has = () => {
+
+    }
+
+    this.remove = () => {
+
+    }
+
+    this.length = () => {
+
+    }
+
+    this.clear = () => {
+      this.bucketArray = [];
+      for (let i=0; i<this.capacity; i++) { this.bucketArray.push({}) };
+      console.log(JSON.stringify(this.bucketArray))
+
+    }
+
+    this.keys = () => {
+
+    }
+
+    this.values = () => {
+
+    }
+
+    this.entries = () => {
+
+    }
+
+  };
+};
 
